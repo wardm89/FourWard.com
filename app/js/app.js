1d11982250df70b21062f75da60175916b0aa139
Matthew Ward <mward62@uncc.edu>
2014-08-13 15:02:53 -0400

    Added header slideshow

--- app/js/app.js
+++ app/js/app.js
@@ -17,3 +17,15 @@ var templateController = function($scope){
 	];
 	
 }
+
+function MainController($scope, $timeout) {
+ var slidesInSlideshow = 3;
+ var time = 5000; 
+  
+  $scope.slideshow = 1;
+  var slideTimer =
+    $timeout(function interval() {
+      $scope.slideshow = ($scope.slideshow % slidesInSlideshow) + 1;
+      slideTimer = $timeout(interval, time);
+    }, time);
+}
